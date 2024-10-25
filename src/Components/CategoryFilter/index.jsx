bd4f642ce73420f9a7a24b1baf947cf5d5801c95
Henry Tottle <henry@Henrys-MacBook-Air.local>
2024-10-25 22:26:42 +0100

    added category drop down

--- src/Components/CategoryFilter/index.jsx
+++ src/Components/CategoryFilter/index.jsx
@@ -1,5 +1,17 @@
+import {useEffect, useState} from "react";
 
 function CategoryFilter ({setSelected}) {
+    const [categories, setCategories] = useState([])
+    const getCategories = async () => {
+        let response = await fetch(`http://localhost:8080/categories`)
+        let json = await response.json()
+        setCategories(json.data)
+        console.log(categories)
+    }
+
+    useEffect(() => {
+        getCategories()
+    }, []);
     const clickHandler = (e) => {
         setSelected(e.target.value)
     }
@@ -9,6 +21,9 @@ function CategoryFilter ({setSelected}) {
             <select onChange={clickHandler} className='text-center m-1 rounded-2xl text-2xl'>
                 <option  value='books'>All Books</option>
                 <option  value='fives'>Highest Rated Books</option>
+                {categories.map((category, i) => {
+                    return <option key={i}>{category.genre_1}</option>
+                })}
             </select>
         </div>
 
